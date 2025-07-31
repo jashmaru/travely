151dc308e2ac8e97a09ce890970bc752d9a3c25a
Jash Maru <jashmaru10@gmail.com>
2025-08-01 01:56:11 +0530

    Removed Type Prop From Card Component & Displaying Popular Tour Cards Using Card Component By Creating Object Of Arrays With Object Containing Values Of Props

--- client/src/pages/Home/PopularTours.jsx
+++ client/src/pages/Home/PopularTours.jsx
@@ -1,74 +1,54 @@
+import TripCard from "../../components/Card";
+
 export default function PopularTours()
 {
+    const popularTourData = [{
+        Name : "Bali,Indonesia",
+        ImgURL : "https://plus.unsplash.com/premium_photo-1677829177642-30def98b0963?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        Duration: "8D | 7N",
+        Price : "$600",
+        Id : 1
+    },
+    {
+        Name : "Atlantis,Dubai",
+        ImgURL : "https://images.unsplash.com/photo-1634148551170-d37d021e0cc9?q=80&w=1184&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        Duration: "4D | 3N",
+        Price : "$500",
+        Id : 2
+    },
+    {
+        Name : "Danang, Vietnam",
+        ImgURL : "https://images.unsplash.com/photo-1555979864-7a8f9b4fddf8?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        Duration: "6D | 5N",
+        Price : "$500",
+        Id : 3
+    },
+    {
+        Name : "Tiger Nest, Bhutan",
+        ImgURL : "https://images.unsplash.com/photo-1578556881786-851d4b79cb73?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        Duration: "7D | 6N",
+        Price : "$400",
+        Id : 4
+    }];
+
     return(
         <>
         <div className="Popular-Tours h-[70vh] w-full flex-col bg-white p-[50px] ">
 
+            {/* Heading Of Section */}
             <div className="Heading font-sans text-4xl font-bold">Popular Tours</div>
 
             {/* <!-- Area for multiple cards --> */}
             <div className="Tour-Cards-Area mt-8 flex h-full w-full flex-wrap gap-8">
-
-                {/* <!-- Card that is to be dispalyed --> */}
-                <div className="Card h-[300px] w-[280px] flex-col hover:scale-[103%] transition-transform ease-in-out duration-300
-                 overflow-hidden rounded-3xl shadow-xl backdrop-blur-xl">
-
-                    {/* <!-- Container 1 --> */}
-                    <div className="Card-Image">
-                        <img className="h-[160px] w-full object-cover" 
-                        src="https://plus.unsplash.com/premium_photo-1677829177642-30def98b0963?q=80&w=1170&
-                        auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
-                    </div>
-
-                    <div className="Tour-Details bg-white p-4">
-                        {/* <!-- Container 2 --> */}
-                        <div className="Tour-Name-Time h-[70px] w-full">
-                        <h1 className="text-lg font-bold">Bali,Indonesia</h1>
-                        <p className="text-sm text-gray-700">8D | 7N</p>
-                        </div>
-
-                        {/* <!-- Container 3 --> */}
-                        <div className="Tour-Price-CTA flex h-[70px] w-full justify-between">
-                            <p className="font-bold text-orange-400">$800</p>
-                            <span><button className="cursor-pointer rounded-lg bg-orange-500 pt-1 
-                            pr-2 pb-1 pl-2 text-white">Book Now</button></span>
-                        </div>
-                    </div>
-
-                </div>
-
-                
-                {/* <!-- Card that is to be dispalyed --> */}
-                <div className="Card h-[300px] w-[280px] flex-col hover:scale-[103%] transition-transform ease-in-out duration-300 
-                overflow-hidden rounded-3xl shadow-xl backdrop-blur-xl">
-
-                    {/* <!-- Container 1 --> */}
-                    <div className="Card-Image">
-                        <img className="h-[160px] w-full object-cover" 
-                        src="https://images.unsplash.com/photo-1634148551170-d37d021e0cc9?q=80&w=1184&auto=format&
-                        fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
-                    </div>
-
-                    <div className="Tour-Details bg-white p-4">
-                        {/* <!-- Container 2 --> */}
-                        <div className="Tour-Name-Time h-[70px] w-full">
-                        <h1 className="text-lg font-bold">Atlantis,Dubai</h1>
-                        <p className="text-sm text-gray-700">4D | 3N</p>
-                        </div>
-
-                        {/* <!-- Container 3 --> */}
-                        <div className="Tour-Price-CTA flex h-[70px] w-full justify-between">
-                            <p className="font-bold text-orange-400">$1000</p>
-                            <span><button className="cursor-pointer rounded-lg bg-orange-500 pt-1 
-                            pr-2 pb-1 pl-2 text-white">Book Now</button></span>
-                        </div>
-                    </div>
-
-                </div>
-                
+                {popularTourData.map((tour,index)=>{
+                    return(
+                        <TripCard key={index} Name={tour.Name} ImgURL={tour.ImgURL} 
+                        Duration={tour.Duration} Price={tour.Price} Id={tour.Id} />
+                    )
+                })}
             </div>
-        </div>
 
+        </div>
         </>
     )
 }

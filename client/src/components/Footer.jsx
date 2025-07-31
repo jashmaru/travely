66803c96e619fc457d3c148578237f5482b4b7cf
Jash Maru <jashmaru10@gmail.com>
2025-08-01 02:32:27 +0530

    Footer Heigh Changed

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer()
 {
     return(
         <>
-        <div className="Footer w-full h-[60vh] bg-orange-50 relative ">
+        <div className="Footer w-full h-[50vh] bg-orange-50 relative ">
             
         </div>
         </>

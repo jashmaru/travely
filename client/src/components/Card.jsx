438365a3a5ec77df802cf1b5bd3abad6ec8520b5
Jash Maru <jashmaru10@gmail.com>
2025-08-07 14:08:33 +0530

    Testimonial Section Tested With The Card Component Of Testimonial

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -5,26 +5,26 @@ import { Link } from "react-router-dom";
     return(
         <>
         {/*<!-- Card that is to be dispalyed -->*/}
-        <div classNameName="Card h-[300px] flex-col hover:scale-[103%] transition-transform ease-in-out duration-300
+        <div className="Card h-[300px] flex-col hover:scale-[103%] transition-transform ease-in-out duration-300
          overflow-hidden rounded-3xl shadow-xl backdrop-blur-xl">
 
             {/*<!-- Container 1 -->*/}
-            <div classNameName="Card-Image">
-                <img classNameName="h-[160px] w-full object-cover" src={ImgURL} alt={Name} />
+            <div className="Card-Image">
+                <img className="h-[160px] w-full object-cover" src={ImgURL} alt={Name} />
             </div>
 
-            <div classNameName="Tour-Details bg-white p-4">
+            <div className="Tour-Details bg-white p-4">
                 
                 {/*<!-- Container 2 -->*/}
-                <div classNameName="Tour-Name-Time h-[70px] w-full">
-                    <h1 classNameName="text-lg font-bold">{Name}</h1>
-                    <p classNameName="text-sm text-gray-700">{Duration}</p>
+                <div className="Tour-Name-Time h-[70px] w-full">
+                    <h1 className="text-lg font-bold">{Name}</h1>
+                    <p className="text-sm text-gray-700">{Duration}</p>
                 </div>
 
                 {/*<!-- Container 3 -->*/}
-                <div classNameName="Tour-Price-CTA flex h-[70px] w-full justify-between">
-                    <p classNameName="font-bold text-orange-400">₹ {Price.toLocaleString("en-IN")}</p>
-                    <span><Link to={"/"+Type+"/"+Id}><button classNameName="cursor-pointer rounded-lg bg-orange-500 pt-1 
+                <div className="Tour-Price-CTA flex h-[70px] w-full justify-between">
+                    <p className="font-bold text-orange-400">₹ {Price.toLocaleString("en-IN")}</p>
+                    <span><Link to={"/"+Type+"/"+Id}><button className="cursor-pointer rounded-lg bg-orange-500 pt-1 
                     pr-2 pb-1 pl-2 text-white">Book Now</button></Link></span>
                 </div>
 
@@ -37,20 +37,22 @@ import { Link } from "react-router-dom";
 
  export function TestimonialCard({Date,Review,Rating,Name,ImgURL})
  {
-    <>
-    <div className="Card p-[5px] border relative flex h-[260px] flex-col overflow-hidden rounded-lg">
-        <div className="Person-Details text-gray-900 flex h-[90px] items-center p-[15px] pb-0">
-            <img src={ImgURL} 
-            className="h-[60px] w-[60px] rounded-[200px] object-cover" />
-            <div className="Detail ml-3">
-                <div>{Name}</div>
-                <div>{Rating}</div>
+    return(
+            <>
+            <div className="Card p-[5px] border relative flex h-[260px] flex-col overflow-hidden rounded-lg">
+                <div className="Person-Details text-gray-900 flex h-[90px] items-center p-[15px] pb-0">
+                    <img src={ImgURL} 
+                    className="h-[60px] w-[60px] rounded-[200px] object-cover" />
+                    <div className="Detail ml-3">
+                        <div>{Name}</div>
+                        <div>{Rating}</div>
+                    </div>
+                </div>
+                <div className="Review-Message text-gray-800 flex flex-col h-[180px] text-sm p-[20px]">
+                    <div>{Review}</div> 
+                    <div className="text-[12px] italic text-right mt-2">– {Date}</div>
+                </div>
             </div>
-        </div>
-        <div className="Review-Message text-gray-800 flex flex-col h-[180px] text-sm p-[20px]">
-            <div>{Review}</div> 
-            <div className="text-[12px] italic text-right mt-2">– {Date}</div>
-        </div>
-    </div>
-    </>
+            </>
+    )
  }
--- client/src/pages/Home/Testimonial.jsx
+++ client/src/pages/Home/Testimonial.jsx
@@ -1,12 +1,41 @@
+import { TestimonialCard } from "../../components/Card";
+
 export default function TestimonialSection()
 {
+
+
+    const testimonialData = [
+        {
+            name : "Sweden",
+            imgurl : "https://t3.ftcdn.net/jpg/02/99/04/20/360_F_299042079_vGBD7wIlSeNl7vOevWHiL93G4koMM967.jpg",
+            rating : "⭐⭐⭐⭐⭐",
+            review : "Our Bali tour was nothing short of magical. From the breathtaking beaches to the serene temples, every moment was perfectly planned.",
+            date : "July 23, 2025"
+        },
+        {
+            name : "Joey",
+            imgurl : "https://media.istockphoto.com/id/1389348844/photo/studio-shot-of-a-beautiful-young-woman-smiling-while-standing-against-a-grey-background.jpg?s=612x612&w=0&k=20&c=anRTfD_CkOxRdyFtvsiPopOluzKbhBNEQdh4okZImQc=",
+            rating : "⭐⭐⭐⭐",
+            review : "Wonderful experience in Bali! The guide was super friendly, and we got to explore hidden gems. Only wish we had one more day.",
+            date : "September 23, 2025"
+        },
+        {
+            name : "Karan",
+            imgurl : "https://randomuser.me/api/portraits/men/32.jpg",
+            rating : "⭐⭐⭐⭐⭐",
+            review : "Absolutely flawless from start to finish. The accommodations were top-notch, and the sunset views were breathtaking.",
+            date : "August 10, 2025"
+        }
+    ];
+
+
     return(
         <>
             <div className="Testimonial-Section flex flex-col w-full gap-8 min-h-[70vh] p-[50px] lg:p-[55px]  ">
 
                 <div className="Heading font-bold text-4xl font-sans ">Testimonial Section</div>
                 
-                <div className="Testimonial-Cards-Area flex flex-grow items-center justify-between ">
+                <div className="Testimonial-Cards-Area flex flex-grow gap-8 items-center justify-between ">
 
                     <div className="Left Arrow hover:cursor-pointer ">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} 
@@ -15,8 +44,11 @@ export default function TestimonialSection()
                         </svg>
                     </div>
 
-                    <div className="Testimonial-Card w-full grid grid-cols-3 ">
-                        
+                    <div className="Testimonial-Card gap-4 w-full grid grid-cols-3 ">
+                        {testimonialData.map((data,index)=>{
+                            return <TestimonialCard key={index} Name={data.name} ImgURL={data.imgurl} Date={data.date} Review={data.review} 
+                            Rating={data.rating} />
+                        })}
                     </div>
                      
                     <div className="Right Arrow hover:cursor-pointer">
